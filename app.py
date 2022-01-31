902c936750f1bf99bd8422e0f843a1018369082a
alexsandro-matias <matiasalexsandro@gmail.com>
2022-01-31 12:19:14 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from tkinter import *
+menu_inicial = Tk()
+menu_inicial.title("Autoinstalador de Programas")
+menu_inicial.geometry("500x250+200+200")
+menu_inicial.resizable(True, False)
+
+# menu_inicial.minsize(500,250)
+# menu_inicial.maxsize(700,400)
+
+# menu_inicial.state("zoomed")
+# menu_inicial.state("iconic")
+
+menu_inicial.iconbitmap("icons/regua.ico")
+
+
+menu_inicial.mainloop()
